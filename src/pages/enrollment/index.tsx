7edf1005eb3d1908d11ff78298298f53476b4897
Marco Mesen <marcokse@hotmail.es>
2021-05-22 17:26:59 -0600

    Fist step UI

--- src/pages/enrollment/index.tsx
+++ src/pages/enrollment/index.tsx
@@ -1,9 +1,39 @@
 import { ReactElement } from 'react';
+import { Input } from '@components/base/Input';
+import { Subtitle } from '@components/base/Subtitle';
+import { useRouter } from 'next/router';
 
 export default function PersonalData(): ReactElement {
+  const router = useRouter();
+
+  const handleSubmit: React.FormEventHandler = (event) => {
+    event.preventDefault();
+    // TODO: advance steps on state
+    router.push('/enrollment/health-profile');
+  };
+
   return (
-    <>
-      <div>Enrollment</div>
-    </>
+    <form onSubmit={handleSubmit} className="flex flex-col">
+      <Subtitle text="Personal Data" />
+      <div className="grid md:gap-8 md:grid-cols-2 md:mb-12">
+        <Input label="Name" id="person-name" />
+        <Input label="Last name" id="person-last-name" />
+        <Input label="Gender" id="person-gender" />
+        <Input label="Birthday" id="person-birthday" type="date" max={new Date().toISOString().slice(0, 10)} />
+        <Input label="Email" id="person-email" type="email" />
+        <Input label="Phone number" id="person-phone-number" />
+        <Input label="Marital status" id="person-marital-status" />
+      </div>
+      <Subtitle text="Address" />
+      <div className="grid md:gap-8  md:grid-cols-2 ">
+        <Input label="City" id="person-address-city" />
+        <Input label="State" id="person-address-state" />
+        <Input label="Zip" id="person-address-zip-code" />
+        <Input label="Street Address" id="person-address-street" className="md:col-span-2" />
+      </div>
+      <button className="btn-primary self-end mt-8" type="submit">
+        Continuar
+      </button>
+    </form>
   );
 }
