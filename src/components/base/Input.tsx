047e9020970ec685e4d23ab9b90a83d6702708dc
Marco Mesen <marcokse@hotmail.es>
2021-05-23 14:03:23 -0600

    Input component updated, to handle ref

--- src/components/base/Input.tsx
+++ src/components/base/Input.tsx
@@ -3,6 +3,7 @@ import { ReactElement } from 'react';
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   id: string;
   label?: string;
+  elementRef?: React.RefObject<HTMLInputElement>;
   type?: 'text' | 'number' | 'email' | 'date';
 }
 /**
@@ -11,7 +12,7 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
  * @param id Input id, using to link label
  * @param type This input could get text or number
  */
-export const Input = ({ label, type = 'text', id, className, ...rest }: Props): ReactElement => {
+export const Input = ({ label, type = 'text', id, className, elementRef, ...rest }: Props): ReactElement => {
   return (
     <div className={`py-1 ${className}`}>
       <div className="relative h-10">
@@ -19,6 +20,7 @@ export const Input = ({ label, type = 'text', id, className, ...rest }: Props): 
           id={id}
           type={type}
           name={id}
+          ref={elementRef}
           className="input mt-1 w-full outline-none h-8 px-1"
           placeholder=" "
           {...rest}
