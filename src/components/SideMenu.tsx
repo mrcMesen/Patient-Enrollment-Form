b713a332c5be7ece31a91ab5a69fa9cad1187759
Marco Mesen <marcokse@hotmail.es>
2021-05-23 17:00:57 -0600

    SideMenu updated to handle indicador in responsive view

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -5,14 +5,14 @@ import { SideMenuItem } from './SideMenuItem';
 export const SideMenu = (): ReactElement => {
   return (
     <section className="w-full md:w-1/3 xl:w-1/4 min-w-max md:h-screen md:fixed md:left-0 p-4 md:p-10 bg-green-900 text-white flex flex-col">
-      <div className="mb-8">
+      <div className="md:mb-8">
         <h2 className="text-4xl mb-2">Parsley Health</h2>
-        <p className="text-gray-300">Patient Enrollment</p>
+        <p className="ml-1 md:ml-0 text-gray-300">Patient Enrollment</p>
       </div>
-      <div className="hidden md:block flex-1">
+      <div className="mb-8 flex md:block md:flex-1">
         {[steps.map((step, index) => <SideMenuItem key={step.step} itemNumber={index} title={step.step} />)]}
       </div>
-      <div>Marco Mesen - Assessment</div>
+      <div className="text-xs md:text-base">Marco Mesen - Assessment</div>
     </section>
   );
 };
--- src/components/SideMenuItem.tsx
+++ src/components/SideMenuItem.tsx
@@ -14,28 +14,31 @@ export const SideMenuItem = ({ itemNumber, title }: Props): ReactElement => {
   }, [state.step]);
 
   return (
-    <div className="flex items-center mb-4">
-      <div className={`h-10 w-10 rounded-full flex-full-center ${isActive ? 'bg-white text-green-900' : 'border-2'}`}>
-        {isActive ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            strokeWidth="2.5"
-            stroke="currentColor"
-            fill="none"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M5 12l5 5l10 -10" />
-          </svg>
-        ) : (
-          itemNumber + 1
-        )}
+    <>
+      <div className={`md:hidden h-2 w-8 m-1 rounded ${!isActive ? 'bg-white' : 'bg-green-600'}`} />
+      <div className="hidden md:flex items-center mb-4">
+        <div className={`h-10 w-10 rounded-full flex-full-center ${isActive ? 'bg-white text-green-900' : 'border-2'}`}>
+          {isActive ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              strokeWidth="2.5"
+              stroke="currentColor"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M5 12l5 5l10 -10" />
+            </svg>
+          ) : (
+            itemNumber + 1
+          )}
+        </div>
+        <span className="ml-4">{title}</span>
       </div>
-      <span className="ml-4">{title}</span>
-    </div>
+    </>
   );
 };
