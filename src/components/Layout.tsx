212a79e199efa889fc5c2bf11476f11539601508
Marco Mesen <marcokse@hotmail.es>
2021-05-23 16:04:10 -0600

    Side menu fixxed and responsive

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,9 +9,9 @@ export const Layout = ({ children }: { children: ReactNode }): ReactElement => {
       <Head>
         <title>Enrollment - Personal Data</title>
       </Head>
-      <div className="flex flex-wrap">
+      <div className="flex flex-wrap md:justify-end">
         <SideMenu />
-        <main className="w-2/3 xl:w-3/4 p-10">{children}</main>
+        <main className="w-full md:w-2/3 xl:w-3/4 px-4 md:p-10">{children}</main>
       </div>
     </>
   );
--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -4,12 +4,12 @@ import { SideMenuItem } from './SideMenuItem';
 
 export const SideMenu = (): ReactElement => {
   return (
-    <section className="w-1/3 xl:w-1/4 min-w-max md:h-screen p-10 bg-green-900 text-white flex flex-col">
+    <section className="w-full md:w-1/3 xl:w-1/4 min-w-max md:h-screen md:fixed md:left-0 p-4 md:p-10 bg-green-900 text-white flex flex-col">
       <div className="mb-8">
         <h2 className="text-4xl mb-2">Parsley Health</h2>
         <p className="text-gray-300">Patient Enrollment</p>
       </div>
-      <div className="flex-1">
+      <div className="hidden md:block flex-1">
         {[steps.map((step, index) => <SideMenuItem key={step.step} itemNumber={index} title={step.step} />)]}
       </div>
       <div>Marco Mesen - Assessment</div>
